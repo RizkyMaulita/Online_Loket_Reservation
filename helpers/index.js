f4fd7427e2fcb0eb85ab9ba93a2c317873c59f28
RizkyMaulita <rizky.maulita@gmail.com>
2021-05-02 16:39:52 +0700

    api for endpoint /transaction/get_info/:transaction_id

--- helpers/index.js
+++ helpers/index.js
@@ -2,5 +2,7 @@ module.exports = {
   getCurrentDate: require('./dateHelper.js').getCurrentDate,
   detailStatus: require('./dataHelper.js').detailStatus,
   checkTypeTicket: require('./dataHelper.js').checkTypeTicket,
-  dateComparisonFromNow: require('./dateHelper.js').dateComparisonFromNow
+  dateComparisonFromNow: require('./dateHelper.js').dateComparisonFromNow,
+  convertDateToString: require('./dateHelper.js').convertDateToString,
+  convertRupiah: require('./dataHelper.js').convertRupiah
 }
